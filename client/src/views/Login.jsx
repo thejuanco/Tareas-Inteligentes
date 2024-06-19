39f9fdecf866511b7bae7e0febb91a7a2e05ccc0
Juanco <juanffo874@gmail.com>
2024-06-19 08:52:12 -0600

    Primeros pasos con el formulario y estilos

---
+++ client/src/views/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="grid grid-rows rounded-lg border border-gray-300 w-1/3 mt-14">
+        <h1 className="m-6 font-semibold text-2xl mt-6">Iniciar Sesión</h1>
+        <h3 className="mx-6 text-gray-500 ">
+          Ingresa tu nombre de usuario y contraseña para acceder a tu cuenta
+        </h3>
+        <form className="m-6 mt-16">
+          <label htmlFor="userName" className="font-semibold">Nombre de usuario</label>
+          <input
+            type="email"
+            id="userName"
+            for="userName"
+            placeholder="Ingresa tu nombre de usuario"
+            className="w-full border border-gray-300 rounded-lg py-3 px-2 mt-2 my-6"
+          />
+
+          <label className="font-semibold mt-6">Contraseña</label>
+          <input className="w-full border border-gray-300 rounded-lg py-3 px-2 mt-2" placeholder="******" type="password"/>
+
+          <button className="w-full bg-gray-900 text-white py-3 mt-4 rounded-3xl hover:bg-gray-800">Iniciar Sesión</button>
+          <Link href="#" className="text-center text-gray-500 my-4">¿No tienes cuenta?, Registrate</Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
