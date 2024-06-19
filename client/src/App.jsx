78ef9ecc7b087c2b2ae8f78c3f5073b6e487bdf3
Juanco <juanffo874@gmail.com>
2024-06-18 22:26:39 -0600

    Agregando el Router al main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import {Route, Routes} from 'react-router-dom'
 
 //Importando vistas
 import Home from './views/Home'
+// import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' Component={<Home/>}/>
+        <Route path='/' element={<Home/>} />
       </Routes>
     </>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//import { Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+  </Router>
 )
