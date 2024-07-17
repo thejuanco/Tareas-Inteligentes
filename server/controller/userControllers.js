a29bd011814c1e648709c1d418e067063936cbd7
Juanco <juanffo874@gmail.com>
2024-07-16 23:39:56 -0600

    Primeros pasos para crear un usuario

--- server/controller/userControllers.js
+++ server/controller/userControllers.js
@@ -4,33 +4,28 @@ export const loginUser = (req, res) => {
     return res.json({message: "Desde el login"})
 }
 
-export const registerUser = (req, res) => {
-    return res.json({message: "Desde el registro"})
+export const registerUser = async (req, res) => {
+    try {
+      // Obteniendo los datos del body
+      const { name, lastName, email, password } = req.body;
+
+      const newUser = await User.create({
+        name,
+        lastName,
+        email,
+        password,
+      });
+
+      console.log(`Se creo al usuario: ${newUser}`);
+
+      res.json({name: newUser.name, lastName: newUser.lastName})
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
 
 }
 
 export const ping = async (req, res) => {
     res.json({message: 'Hola soy un ping'})
 
-    const newUser = await User.create({
-        name: 'John',
-        lastName: 'Jara',
-        email: 'john@gmail.com',
-        password: 'password123',
-
-    })
-
-    const user1 = await User.findOne({where: {email: newUser.email}})
-
-    try {
-        if(user1){
-            console.log(`El usuario: ${user1} ya existe`)
-            return;
-        } else {
-            await newUser.save()
-            console.log('Se guardo correctamente', newUser)
-        }
-    } catch (error) {
-        console.log(error)
-    }
 }
