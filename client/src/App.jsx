7b0b9b0d0dca3d90e1f8baad889baf309a0ceb73
Juanco <juanffo874@gmail.com>
2024-07-17 22:37:28 -0600

    Añadiendo el formulario a las rutas del cliente, y dandole estilos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 //Importando vistas
 import Home from './views/Home'
 import Login from './views/Login'
+import Register from './views/auth/Register'
 import NotFound from './views/NotFound'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </>
--- client/src/views/auth/Register.jsx
+++ client/src/views/auth/Register.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <form>
-        
-      </form>
-    </div>
-  )
-}
+    <div className="flex justify-center items-center">
+      <div className="w-1/3 flex-row m-8 border border-gray-300 rounded-lg p-6">
+        <h1 className="font-bold text-center text-3xl">Registro</h1>
+        <p className="text-gray-500 text-center my-2">
+          Ingresa tus datos para crear una cuenta
+        </p>
+        <form className="">
+          <label className="mt-6 my-1 block text-gray-900">Nombre</label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-lg"
+          />
 
-export default Register
+          <label className="mt-6 my-1 block">Apellido</label>
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded-lg"
+          />
+
+          <label className="mt-4 my-1 block">Correo electronico</label>
+          <input
+            type="email"
+            className="w-full p-2 border border-gray-300 rounded-lg"
+          />
+
+          <label className="mt-4 my-1 block">Contraseña</label>
+          <input
+            type="password"
+            className="w-full p-2 border border-gray-300 rounded-lg"
+          />
+
+          <button className="w-full py-2 bg-gray-900 text-white rounded-lg font-semibold mt-6 hover:bg-gray-800">
+            Registrarse
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
