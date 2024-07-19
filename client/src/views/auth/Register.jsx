6b97287c3a3206243324ed18d4be07c528858a5a
Juanco <juanffo874@gmail.com>
2024-07-18 23:00:19 -0600

    Añadiendo las primeras validaciones

--- client/src/views/auth/Register.jsx
+++ client/src/views/auth/Register.jsx
@@ -16,46 +16,72 @@ const Register = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="w-1/3 flex-row m-8 border border-gray-300 rounded-lg p-6">
+      <div className="md:w-1/3 sm:w-1/2 flex-row m-8 border border-gray-300 rounded-lg p-6">
         <h1 className="font-bold text-center text-3xl">Registro</h1>
         <p className="text-gray-500 text-center my-2">
           Ingresa tus datos para crear una cuenta
         </p>
         <form className="" onSubmit={handleSubmit(onSubmit)}>
-          <label className="mt-6 my-1 block text-gray-900 font-semibold" htmlFor="nameUser">Nombre</label>
+          <label
+            className="mt-6 my-1 block text-gray-900 font-semibold"
+            htmlFor="nameUser"
+          >
+            Nombre
+          </label>
           <input
             name="nameUser"
             type="text"
             className="w-full p-2 border border-gray-300 rounded-lg"
-            {...register("name")}
+            {...register("name", { required: true })}
           />
+          {errors.name?.type === "required" && (
+            <p className="text-red-700">El nombre el obligatorio</p>
+          )}
 
-          <label className="mt-6 my-1 block font-semibold" htmlFor="emailUser">Apellido</label>
+          <label className="mt-6 my-1 block font-semibold" htmlFor="emailUser">
+            Apellido
+          </label>
           <input
             name="emailUser"
             type="text"
             className="w-full p-2 border border-gray-300 rounded-lg"
-            {...register("lastname")}
+            {...register("lastname", {required: true})}
           />
+          {errors.lastname?.type === "required" && (
+            <p className="text-red-700">El apellido es obligatorio</p>
+          )}
 
-          <label className="mt-4 my-1 block font-semibold">Correo electronico</label>
+          <label className="mt-4 my-1 block font-semibold">
+            Correo electronico
+          </label>
           <input
             type="email"
             className="w-full p-2 border border-gray-300 rounded-lg"
-            {...register("email")}
+            {...register("email", {required: true})}
           />
+          {errors.email?.type === "required" && (
+            <p className="text-red-700">El correo es obligatorio</p>
+          )}
 
-          <label className="mt-4 my-1 block font-semibold" htmlFor="passUser">Contraseña</label>
+          <label className="mt-4 my-1 block font-semibold" htmlFor="passUser">
+            Contraseña
+          </label>
           <input
             name="passUser"
             id="passUser"
             placeholder="******"
             type="password"
             className="w-full p-2 border border-gray-300 rounded-lg"
-            {...register("password")}
+            {...register("password", {required: true})}
           />
+          {errors.password?.type === "required" && (
+            <p className="text-red-700">La contraseña es obligatorio</p>
+          )}
 
-          <button className="w-full py-2 bg-gray-900 text-white rounded-lg font-semibold mt-6 hover:bg-gray-800" type="submit">
+          <button
+            className="w-full py-2 bg-gray-900 text-white rounded-lg font-semibold mt-6 hover:bg-gray-800"
+            type="submit"
+          >
             Registrarse
           </button>
         </form>
